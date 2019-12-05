7d893f0b12114d652e2f329042f47e5c21c53cc7
root <root@root.com>
2019-12-05 18:52:40 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import router from './router'
 import store from './store'
 import toast from "components/common/toast";
 
+import FastClick from 'fastclick'
 
 Vue.config.productionTip = false
 //添加消息总线
 Vue.prototype.$bus = new Vue()
 //安装toast插件
 Vue.use(toast)
+
+//解决移动端300ms延迟
+FastClick.attach(document.body)
 
 new Vue({
   render: h => h(App),
